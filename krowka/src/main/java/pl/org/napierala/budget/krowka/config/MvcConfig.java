7951f6241196f60a92b4a171d9a7c889327cc235
Szymon Napierała <napierala.szymon@gmail.com>
2016-04-05 19:22:50 +0200

    Przeniesienie konfiguracji i18n i l10n do MvcConfig

--- krowka/src/main/java/pl/org/napierala/budget/krowka/config/MvcConfig.java
+++ krowka/src/main/java/pl/org/napierala/budget/krowka/config/MvcConfig.java
@@ -1,15 +1,26 @@
 package pl.org.napierala.budget.krowka.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
-public class MvcConfig {
+@EnableWebMvc
+public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public TemplateResolver templateResolver() {
@@ -34,6 +45,30 @@ public class MvcConfig {
 		viewResolver.setTemplateEngine(this.templateEngine());
 		viewResolver.setCharacterEncoding("UTF-8");
 		return viewResolver;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("pl"));
+		return localeResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource =
+				new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:/i18n/app");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		super.addInterceptors(registry);
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		registry.addInterceptor(interceptor);
 	}
 
 }
