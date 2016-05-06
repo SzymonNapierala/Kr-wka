2ecb3f4b2960a90b8d5985e6aed759b3e82b4705
Szymon Napierała <napierala.szymon@gmail.com>
2016-05-06 18:16:58 +0200

    Przenisienie konfiguracji uwierzytelnienia do JavaConfig + CSRF token

---
+++ krowka/src/main/java/pl/org/napierala/budget/krowka/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package pl.org.napierala.budget.krowka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import pl.org.napierala.budget.krowka.filter.CsrfHeaderFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http.authorizeRequests().antMatchers("/resources/**", "/login").permitAll()
+		 		.anyRequest().authenticated().and()
+		 		.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+		 		.csrf().csrfTokenRepository(this.getCsrfTokenRepository());
+		 http.formLogin().loginPage("/login").loginProcessingUrl("/login")
+		 		.defaultSuccessUrl("/").failureUrl("/login?error=true")
+		 		.usernameParameter("username").passwordParameter("password");
+	}
+
+	private CsrfTokenRepository getCsrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+
+}
